504ed1131c75a820c58d9dd179d5ac92d975241d
CrazyBene <benedikt.grau@gmx.de>
2023-12-03 15:57:10 +0100

    Solve the puzzle of day 3

---
+++ src/Day03.kt
@@ -0,0 +1,95 @@
+fun main() {
+    data class Position(val x: Int, val y: Int)
+
+    data class EngineSchematicNumber(val number: Int, val startPosition: Position) {
+
+        fun getNeighborPositions(): Set<Position> {
+            val neighbors = mutableSetOf<Position>()
+            for (x in startPosition.x - 1..startPosition.x + number.toString().length) {
+                neighbors += Position(x, startPosition.y - 1)
+                neighbors += Position(x, startPosition.y + 1)
+            }
+            neighbors += Position(startPosition.x - 1, startPosition.y)
+            neighbors += Position(startPosition.x + number.toString().length, startPosition.y)
+            return neighbors
+        }
+
+    }
+
+    data class Symbol(val symbol: Char, val position: Position)
+
+    fun parseInput(input: List<String>): Pair<List<EngineSchematicNumber>, List<Symbol>> {
+        val engineSchematicNumbers = mutableListOf<EngineSchematicNumber>()
+        val symbols = mutableListOf<Symbol>()
+
+        input.forEachIndexed { lineNumber, line ->
+            var currentNumberString = ""
+            var startPosition: Position? = null
+            line.forEachIndexed { colNumber, char ->
+                if (char.isDigit()) {
+                    currentNumberString += char
+                    if (startPosition == null) startPosition = Position(colNumber, lineNumber)
+                } else if (currentNumberString.isNotEmpty()) {
+                    engineSchematicNumbers += EngineSchematicNumber(currentNumberString.toInt(), startPosition!!)
+                    currentNumberString = ""
+                    startPosition = null
+                }
+
+                if (!char.isDigit() && char != '.') symbols += Symbol(char, Position(colNumber, lineNumber))
+            }
+            if (currentNumberString.isNotEmpty()) {
+                engineSchematicNumbers += EngineSchematicNumber(currentNumberString.toInt(), startPosition!!)
+                currentNumberString = ""
+                startPosition = null
+            }
+        }
+
+        return engineSchematicNumbers to symbols
+    }
+
+    fun part1(input: List<String>): Int {
+        val (engineSchematicNumbers, symbols) = parseInput(input)
+        val symbolPositions = symbols.map { it.position }
+
+        return engineSchematicNumbers.filter { engineSchematicNumber ->
+            engineSchematicNumber.getNeighborPositions().forEach { neighbor ->
+                if (symbolPositions.contains(neighbor)) return@filter true
+            }
+            false
+        }.sumOf { it.number }
+    }
+
+    fun part2(input: List<String>): Int {
+        val (engineSchematicNumbers, symbols) = parseInput(input)
+        val gearSymbolPositions = symbols.filter { it.symbol == '*' }.map { it.position }
+
+        val gearWithNeighbors = mutableMapOf<Position, List<EngineSchematicNumber>>()
+        engineSchematicNumbers.forEach { engineSchematicNumber ->
+            engineSchematicNumber.getNeighborPositions().forEach { neighbor ->
+                if (gearSymbolPositions.contains(neighbor)) {
+                    val symbolNeighbors = gearWithNeighbors[neighbor]?.toMutableList() ?: mutableListOf()
+                    symbolNeighbors.add(engineSchematicNumber)
+                    gearWithNeighbors[neighbor] = symbolNeighbors
+                }
+            }
+        }
+
+        return gearWithNeighbors.filter {
+            it.value.size == 2
+        }.map { (_, value) ->
+            value.map { it.number }.reduce { acc, engineNumber -> acc * engineNumber }
+        }.sum()
+    }
+
+    val testInput = readInput("Day03Test")
+    val input = readInput("Day03")
+
+    println("Advent of Code 2023 - Day 3")
+    println("---------------------------")
+
+    check(part1(testInput) == 4361)
+    println("Solution for part1: ${part1(input)}")
+
+    check(part2(testInput) == 467835)
+    println("Solution for part2: ${part2(input)}")
+}
